5541b3b0c3c92b746de61c52cff670cfce3daf6e
annyconfessor <35703124+AnnyConfessor@users.noreply.github.com>
2019-02-26 17:51:36 -0300

    Add files via upload

---
+++ lista1.2.cpp
@@ -0,0 +1,42 @@
+/*Fazer um programa que recebe um símbolo de operação do usuário (+, -, / ou *) e
+dois números reais. O programa deve retornar o resultado da operação recebida
+sobre estes dois números. Este funcionamento deve ser repetido até que o símbolo
+seja algo que não corresponda a nenhuma das quatro operações reconhecidas. 
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    char a;
+    float b;
+    float c;
+    
+    cout << "Número real: ";
+    cin >> b;
+
+    cout << "Número real";
+    cin >> c;
+
+    cout << "Operação entre os números ('+','-','/'.'*'): ";
+    cin >> a;    
+    } 
+}
+
+int operacao(char a,float b,float c){
+    int result;
+
+    if(a=='+'){
+        result = b + c;
+    } else if(a=='-'){
+        result = b - c;
+    } else if(a=='/'){
+        result = b / c;
+    } else if(a=='*'){
+        result = b * c;
+    } else if(a!='+' || a!='-' || a!='/' || a!='*'){
+        break;
+    }
+
+    return result;
+}
+
